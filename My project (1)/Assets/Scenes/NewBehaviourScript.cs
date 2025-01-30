a0481429228c36c7b32856b6a3cac5fdc8d1fcd8
ArthurDaniel40 <arthurfico56@gmail.com>
2025-01-30 11:50:52 -0300

    cenario

---
+++ My project (1)/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    
+public class player : MonoBehaviour
+{
+    public int numero = 10;
+    
+    void Start()
+    {
+     Debug.Log(message: "testando .1 .2 .1 .2" + numero);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
